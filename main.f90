7f6e7cf7f435f0d69e0b040c8a87c99803f8cae6
Danny Hermes <daniel.j.hermes@gmail.com>
2017-07-30 10:13:15 -0700

    Adding some separators to Fortan output.

--- main.f90
+++ main.f90
@@ -10,25 +10,30 @@ program main
   integer :: size_
   real(dp) :: val(4, 2), two_val(4, 2)
 
+  call print_sep()
+  ! foo()
   bar = 1.0_dp
   baz = 16.0_dp
   call foo(bar, baz, quux)
+  print *, "foo(", bar, baz, ") = ", quux
 
+  call print_sep()
+  ! make_udf()
   buzz = 1.25_dp
   broken = 5.0_dp
   how_many = 1337
   call make_udf(buzz, broken, how_many, quuz)
+  print *, "make_udf(", buzz, broken, how_many, ")"
+  print *, "       = ", quuz
 
+  call print_sep()
+  ! foo_array()
   val = reshape((/ &
        3.0_dp, 1.0_dp, 9.0_dp, -1.0_dp, &
        4.5_dp, 1.25_dp, 0.0_dp, 4.0_dp/), &
        (/4, 2/))
   size_ = size(val, 1)
   call foo_array(size_, val, two_val)
-
-  print *, "foo(", bar, baz, ") = ", quux
-  print *, "make_udf(", buzz, broken, how_many, ")"
-  print *, "       = ", quuz
   print *, "foo_array("
   print *, "    ", size_, ","
   print *, "    [[", val(1, 1), val(1, 2), "],"
@@ -41,4 +46,10 @@ program main
   print *, "     [", two_val(3, 1), two_val(3, 2), "],"
   print *, "     [", two_val(4, 1), two_val(4, 2), "]]"
 
-end
+end program
+
+subroutine print_sep()
+
+  print *, "------------------------------------------------------------"
+
+end subroutine print_sep
