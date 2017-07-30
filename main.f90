03a1a9dcebf3735aa218aca2d9af683ed9e92ea8
Danny Hermes <daniel.j.hermes@gmail.com>
2017-07-30 10:29:09 -0700

    Making the output of the plain Fortran program prettier.
    This was a long time coming, eek.

--- main.f90
+++ main.f90
@@ -1,6 +1,6 @@
 program main
 
-  use example, only: dp, foo, foo_array, make_udf, UserDefined
+  use example, only: dp, foo, foo_array, make_udf, just_print, UserDefined
   implicit none
 
   real(dp) :: bar, baz, quux
@@ -15,7 +15,8 @@ program main
   bar = 1.0_dp
   baz = 16.0_dp
   call foo(bar, baz, quux)
-  print *, "foo(", bar, baz, ") = ", quux
+  write (*, "(A, F8.6, A, F9.6, A, F9.6)") &
+       "quux = foo(", bar, ", ", baz, ") = ", quux
 
   call print_sep()
   ! make_udf()
@@ -23,8 +24,11 @@ program main
   broken = 5.0_dp
   how_many = 1337
   call make_udf(buzz, broken, how_many, quuz)
-  print *, "make_udf(", buzz, broken, how_many, ")"
-  print *, "       = ", quuz
+  write (*, "(A, F8.6, A, F8.6, A, I4, A)") &
+       "quuz = make_udf(", buzz, ", ", broken, ", ", how_many, ")"
+  write (*, "(A, F8.6, A, F8.6, A, I4, A)") &
+       "     = UserDefined(", quuz%buzz, ", ", quuz%broken, &
+       ", ", quuz%how_many, ")"
 
   call print_sep()
   ! foo_array()
@@ -34,22 +38,32 @@ program main
        (/4, 2/))
   size_ = size(val, 1)
   call foo_array(size_, val, two_val)
-  print *, "foo_array("
-  print *, "    ", size_, ","
-  print *, "    [[", val(1, 1), val(1, 2), "],"
-  print *, "     [", val(2, 1), val(2, 2), "],"
-  print *, "     [", val(3, 1), val(3, 2), "],"
-  print *, "     [", val(4, 1), val(4, 2), "]],"
-  print *, ") = "
-  print *, "    [[", two_val(1, 1), two_val(1, 2), "],"
-  print *, "     [", two_val(2, 1), two_val(2, 2), "],"
-  print *, "     [", two_val(3, 1), two_val(3, 2), "],"
-  print *, "     [", two_val(4, 1), two_val(4, 2), "]]"
+  write (*, "(A)"), "foo_array("
+  write (*, "(A, I1, A)"), "    ", size_, ","
+  write (*, "(A, F8.6, A, F8.6, A)"), "    [[", val(1, 1), ", ", val(1, 2), "],"
+  write (*, "(A, F8.6, A, F8.6, A)"), "     [", val(2, 1), ", ", val(2, 2), "],"
+  write (*, "(A, F8.6, A, F8.6, A)"), "     [", val(3, 1), ", ", val(3, 2), "],"
+  write (*, "(A, F9.6, A, F8.6, A)"), &
+       "     [", val(4, 1), ", ", val(4, 2), "]],"
+  write (*, "(A)"), ") = "
+  write (*, "(A, F8.6, A, F8.6, A)"), &
+       "    [[", two_val(1, 1), ", ", two_val(1, 2), "],"
+  write (*, "(A, F8.6, A, F8.6, A)"), &
+       "     [", two_val(2, 1), ", ", two_val(2, 2), "],"
+  write (*, "(A, F9.6, A, F8.6, A)"), &
+       "     [", two_val(3, 1), ", ", two_val(3, 2), "],"
+  write (*, "(A, F9.6, A, F8.6, A)"), &
+       "     [", two_val(4, 1), ", ", two_val(4, 2), "]]"
+
+  call print_sep()
+  ! just_print()
+  write (*, "(A)"), "just_print()"
+  call just_print()
 
 end program
 
 subroutine print_sep()
 
-  print *, "------------------------------------------------------------"
+  write (*, "(A)") "------------------------------------------------------------"
 
 end subroutine print_sep
