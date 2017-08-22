3c18d85bf14ee6911bbe517a2fddd1dba58ed3e7
Danny Hermes <daniel.j.hermes@gmail.com>
2017-08-21 17:18:30 -0700

    Adding make_udf() call to Golang example.

--- golang/main.go
+++ golang/main.go
@@ -17,6 +17,18 @@ func main() {
 	fmt.Printf("quux = foo(%f, %f) = %f\n", bar, baz, quux)
 
 	fmt.Println(separator)
+	// make_udf()
+	buzz := 1.25
+	broken := 5.0
+	howMany := int32(1337)
+	quuz := example.MakeUDF(buzz, broken, howMany)
+	fmt.Printf(
+		"quuz = make_udf(%f, %f, %d)\n",
+		buzz, broken, howMany,
+	)
+	fmt.Printf("     = %T(%v)\n", quuz, quuz)
+
+	fmt.Println(separator)
 	// just_print()
 	fmt.Println("just_print()")
 	example.JustPrint()
--- golang/src/example/example.go
+++ golang/src/example/example.go
@@ -3,16 +3,37 @@ package example
 // #include "example.h"
 import "C"
 
+type UserDefined struct {
+	Buzz    float64
+	Broken  float64
+	HowMany int32
+}
+
 func JustPrint() {
 	C.just_print()
 }
 
 func Foo(bar, baz float64) float64 {
-	var quux C.double
+	var quux float64
 	C.foo(
 		C.double(bar),
 		C.double(baz),
-		&quux,
+		(*C.double)(&quux),
 	)
-	return float64(quux)
+	return quux
+}
+
+func MakeUDF(buzz, broken float64, howMany int32) UserDefined {
+	var quuz C.struct_UserDefined
+	C.make_udf(
+		(*C.double)(&buzz),
+		(*C.double)(&broken),
+		(*C.int)(&howMany),
+		&quuz,
+	)
+	return UserDefined{
+		*(*float64)(&quuz.buzz),
+		*(*float64)(&quuz.broken),
+		*(*int32)(&quuz.how_many),
+	}
 }
