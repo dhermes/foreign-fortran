50b930157bd403aa893bb77cabba820933358297
Danny Hermes <daniel.j.hermes@gmail.com>
2017-09-08 20:57:02 -0700

    Producing universal binary object files on OS X when possible.
    Fixes #3. (But this only builds universal object files if the Python
    being used does as well. So of the three journals, only one is
    updated.)

--- cython/package/setup.py
+++ cython/package/setup.py
@@ -83,12 +83,40 @@ def fortran_search_path(f90_compiler):
 
 
 def patch_library_dirs(f90_compiler):
+    """Patch up ``f90_compiler.library_dirs``.
+
+    This is needed on Mac OS X for a Homebrew installed ``gfortran``, which
+    doesn't come with the correct library directories by default (this is
+    likely unintentional).
+
+    The ``library_dirs`` directory can be over-ridden by using the
+    ``GFORTRAN_LIB`` environment variable. This might be desirable, since the
+    Homebrew ``libgfortran`` is **also** not a universal binary. So this
+    over-ride could be used to point to a custom made ``libgfortran.dylib``
+    that is a combined version of the ``i386`` and ``x86_64`` versions of
+    ``libgfortran`` provided by Homebrew.
+
+    Args:
+        f90_compiler (numpy.distutils.fcompiler.FCompiler): A Fortran compiler
+            instance.
+    """
+    from numpy.distutils.fcompiler import gnu
+
+    # Only Mac OS X.
     if sys.platform != MAC_OS_X:
         return
+    # Only ``gfortran``.
+    if not isinstance(f90_compiler, gnu.Gnu95FCompiler):
+        return
 
+    gfortran_lib = os.environ.get('GFORTRAN_LIB')
     library_dirs = f90_compiler.library_dirs
+
     # Update in place.
-    library_dirs[:] = fortran_search_path(f90_compiler)
+    if gfortran_lib is None:
+        library_dirs[:] = fortran_search_path(f90_compiler)
+    else:
+        library_dirs[:] = [gfortran_lib]
 
 
 def check_dual_architecture():
@@ -100,6 +128,10 @@ def check_dual_architecture():
 
     We use ``lipo -info`` rather than ``file`` because ``lipo`` is
     purpose-built for checking the architecture(s) in a file.
+
+    This property could also be checked by looking for the presence of
+    multiple architectures in
+    ``distutils.sysconfig.get_config_var('LDFLAGS')``.
 
     Returns:
         bool: Indicating if the Python binary is dual architecture
@@ -135,12 +167,12 @@ def gfortran_supports_dual_architecture():
     with open(source_name, 'w') as file_obj:
         file_obj.writelines([
             'subroutine bar(x, y)\n',
-            '  integer, intent(in) :: x\n'
-            '  integer, intent(out) :: y\n'
-            '\n'
-            '  y = x + 2\n'
-            '\n'
-            'end subroutine bar\n'
+            '  integer, intent(in) :: x\n',
+            '  integer, intent(out) :: y\n',
+            '\n',
+            '  y = x + 2\n',
+            '\n',
+            'end subroutine bar\n',
             '\n',
         ])
 
@@ -160,6 +192,183 @@ def gfortran_supports_dual_architecture():
     return result
 
 
+class _DualArchitectureCompile(object):
+    """Callable wrapper that over-rides ``_compile``.
+
+    Only relevant on OS X. Objects of this type are intended to be used
+    to replace / augment to ``_compile`` method on a ``Gnu95FCompiler`` (i.e.
+    a Fortran compiler). This is because the Homebrew ``gfortran`` can't
+    build fat binaries:
+
+    .. code-block:: console
+
+       $ gfortran -arch i386 -arch x86_64 -c bar.f90 -o bar.o
+       gfortran: warning: x86_64 conflicts with i386 (arch flags ignored)
+
+    So instead, this will compile two separate object files and combine them:
+
+    .. code-block:: console
+
+       $ gfortran -arch i386   -c bar.f90 -o ${I386_DIR}/bar.o
+       $ gfortran -arch x86_64 -c bar.f90 -o ${X86_64_DIR}/bar.o
+       $ lipo ${I386_DIR}/bar.o ${X86_64_DIR}/bar.o -create -output bar.o
+
+    Args:
+        f90_compiler (numpy.distutils.fcompiler.gnu.Gnu95FCompiler): A Fortran
+            compiler instance.
+    """
+
+    def __init__(self, f90_compiler):
+        self.f90_compiler = f90_compiler
+        self.original_compile = f90_compiler._compile
+        self.compiler_cmd = f90_compiler.compiler_f90
+        self.arch_index = None  # Set in ``_verify()``.
+        self.arch_value = None  # Set in ``_verify()``.
+        self._verify()
+
+    def _verify(self):
+        """Makes sure the constructor arguments are valid.
+
+        In particular, makes sure that ``f90_compiler`` corresponds to
+        ``gfortran`` and that ``compiler_cmd`` has exactly one instance
+        of ``-arch``.
+
+        If this succeeds, will set ``arch_index`` and ``arch_value`` on
+        the instance.
+
+        Raises:
+            TypeError: If ``compiler_cmd`` is not a ``list``.
+            TypeError: If ``f90_compiler`` is not a ``Gnu95FCompiler``.
+            ValueError: If ``compiler_cmd`` doesn't have exactly one ``-arch``
+                segment.
+            ValueError: If ``-arch`` is the **last** segment in
+                ``compiler_cmd``.
+            ValueError: If the ``-arch`` value is not ``i386`` or ``x86_64``.
+        """
+        from numpy.distutils.fcompiler import gnu
+
+        if not isinstance(self.compiler_cmd, list):
+            raise TypeError('Expected a list', self.compiler_cmd)
+
+        if not isinstance(self.f90_compiler, gnu.Gnu95FCompiler):
+            raise TypeError('Expected a Gnu95FCompiler', self.f90_compiler)
+
+        if self.compiler_cmd.count('-arch') != 1:
+            raise ValueError(
+                'Did not find exactly one "-arch" in', self.compiler_cmd)
+
+        arch_index = self.compiler_cmd.index('-arch') + 1
+        if arch_index == len(self.compiler_cmd):
+            raise ValueError(
+                'There is no architecture specified in', self.compiler_cmd)
+
+        arch_value = self.compiler_cmd[arch_index]
+        if arch_value not in ('i386', 'x86_64'):
+            raise ValueError(
+                'Unexpected architecture', arch_value, 'in', self.compiler_cmd)
+
+        self.arch_index = arch_index
+        self.arch_value = arch_value
+
+    def _set_architecture(self, architecture):
+        """Set the architecture on the Fortran compiler.
+
+        ``compiler_cmd`` is actually a list (mutable), so we can update it here
+        and it will change the architecture that ``f90_compiler`` targets.
+
+        Args:
+            architecture (str): One of ``i386`` or ``x86_64``.
+        """
+        self.compiler_cmd[self.arch_index] = architecture
+
+    def _restore_architecture(self):
+        """Restore the architecture on the Fortran compiler.
+
+        Resets the ``-arch`` value in ``compiler_cmd`` to its original value.
+        """
+        self.compiler_cmd[self.arch_index] = self.arch_value
+
+    def __call__(self, obj, src, ext, cc_args, extra_postargs, pp_opts):
+        """Call-able replacement for ``_compile``.
+
+        This assumes (but does not verify) that ``original_compile`` has
+        no return value.
+
+        Args:
+            obj (str): The location of the object file to be created.
+            src (str): The location of the source file to be compiled.
+            ext (str): The file extension (used to determine flags).
+            cc_args (List[str]): Compile args, typically just ``['-c']``.
+            extra_postargs (List[str]): Extra arguments at the end of the
+                compile command.
+            pp_opts (List[str]): Unused by the NumPy ``distutils`` Fortran
+                compilers. List of pre-processor options.
+        """
+        obj_name = os.path.basename(obj)
+
+        # Create a directory and compile an object targeting i386.
+        i386_dir = tempfile.mkdtemp(suffix='-i386')
+        i386_obj = os.path.join(i386_dir, obj_name)
+        self._set_architecture('i386')
+        self.original_compile(
+            i386_obj, src, ext, cc_args, extra_postargs, pp_opts)
+
+        # Create a directory and compile an object targeting x86_64.
+        x86_64_dir = tempfile.mkdtemp(suffix='-x86_64')
+        x86_64_obj = os.path.join(x86_64_dir, obj_name)
+        self._set_architecture('x86_64')
+        self.original_compile(
+            x86_64_obj, src, ext, cc_args, extra_postargs, pp_opts)
+
+        # Restore the compiler back to how it was before we modified it.
+        self._restore_architecture()
+
+        # Use ``lipo`` to combine the object files into a universal.
+        lipo_cmd = ('lipo', i386_obj, x86_64_obj, '-create', '-output', obj)
+        self.f90_compiler.spawn(lipo_cmd)
+
+        # Clean up the temporary directories.
+        shutil.rmtree(i386_dir)
+        shutil.rmtree(x86_64_dir)
+
+
+def patch_gfortran(f90_compiler):
+    """Modify the Fortran compiler to create universal binary object files.
+
+    Does so by patching ``f90_compiler._compile`` with a custom command.
+
+    Patching is only done if:
+
+    * The platform is OS X
+    * The current compiler is ``gfortran``
+    * The current Python is a universal binary (i.e. dual architecture)
+    * The version of ``gfortran`` cannot create universal binaries
+
+    Args:
+        f90_compiler (numpy.distutils.fcompiler.FCompiler): A Fortran compiler
+            instance.
+    """
+    from numpy.distutils.fcompiler import gnu
+
+    # Only on OS X.
+    if sys.platform != MAC_OS_X:
+        return
+
+    # Only with ``gfortran``.
+    if not isinstance(f90_compiler, gnu.Gnu95FCompiler):
+        return
+
+    # Only if Python is a universal binary.
+    if not check_dual_architecture():
+        return
+
+    # Only if ``gfortran`` can't produce universal binaries.
+    if gfortran_supports_dual_architecture():
+        return
+
+    f90_compiler._compile = _DualArchitectureCompile(f90_compiler)
+
+
 def get_f90_compiler():
     c_compiler = distutils.ccompiler.new_compiler()
     c_compiler.verbose = 2
@@ -171,6 +380,7 @@ def get_f90_compiler():
     f90_compiler.verbose = 2
 
     patch_library_dirs(f90_compiler)
+    patch_gfortran(f90_compiler)
 
     return f90_compiler
 
