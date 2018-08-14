f4d49298ba7f83a8f5f493f2004487b6d2bfa3f2
Danny Hermes <daniel.j.hermes@gmail.com>
2018-08-14 02:16:21 -0700

    De-duplicating in `fortran_search_path.py`.

--- python/fortran_search_path.py
+++ python/fortran_search_path.py
@@ -1,6 +1,19 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import collections
 import os
 import subprocess
 import sys
@@ -27,7 +40,7 @@ def main():
         sys.exit(1)
 
     library_line = library_lines[0]
-    accepted = []
+    accepted = collections.OrderedDict()  # Like an ordered set.
     for part in library_line.split(":"):
         full_path = os.path.abspath(part)
 
@@ -39,9 +52,9 @@ def main():
             print(msg, file=sys.stderr)
             sys.exit(1)
 
-        accepted.append("-L" + full_path)
+        accepted["-L" + full_path] = True
 
-    print(" ".join(accepted))
+    print(" ".join(accepted.keys()))
 
 
 if __name__ == "__main__":
