1c2c3746d0d3eb2a65d949f277d9a925d4c73ee3
Danny Hermes <daniel.j.hermes@gmail.com>
2018-08-14 01:02:38 -0700

    Adding a "stub" for C++ example.
    Also updating the `.cpp` file by running `clang-format` with
    the Google style.

--- cpp/example.cpp
+++ cpp/example.cpp
@@ -5,13 +5,9 @@
 
 #include "example.h"
 
-void print_sep (void)
-{
-  std::cout << std::string(60, '-') << std::endl;
-}
+void print_sep(void) { std::cout << std::string(60, '-') << std::endl; }
 
-int main ()
-{
+int main() {
   // Applies throughout.
   std::cout << std::fixed << std::setprecision(6);
 
@@ -19,9 +15,8 @@ int main ()
   // foo()
   double bar = 1.0, baz = 16.0, quux;
   foo(bar, baz, &quux);
-  std::cout <<
-    "quux = foo(" << bar << ", " << baz << ") = " <<
-    quux << std::endl;
+  std::cout << "quux = foo(" << bar << ", " << baz << ") = " << quux
+            << std::endl;
 
   print_sep();
   // make_udf()
@@ -29,15 +24,10 @@ int main ()
   int how_many = 1337;
   UserDefined quuz;
   make_udf(&buzz, &broken, &how_many, &quuz);
-  std::cout <<
-    "quuz = make_udf(" << buzz << ", " << broken << ", " <<
-    how_many << ")" << std::endl;
-  std::cout <<
-    "     = UserDefined(" <<
-    quuz.buzz << ", " <<
-    quuz.broken << ", " <<
-    quuz.how_many << ")" <<
-    std::endl;
+  std::cout << "quuz = make_udf(" << buzz << ", " << broken << ", " << how_many
+            << ")" << std::endl;
+  std::cout << "     = UserDefined(" << quuz.buzz << ", " << quuz.broken << ", "
+            << quuz.how_many << ")" << std::endl;
 
   print_sep();
   // just_print()
--- docs/cpp.rst
+++ docs/cpp.rst
@@ -2,4 +2,18 @@
 C++
 ###
 
-Placeholder
+The calling script in C++ is only partially complete:
+
+.. code-block:: console
+
+   $ ./cpp_example
+   ------------------------------------------------------------
+   quux = foo(1.000000, 16.000000) = 61.000000
+   ------------------------------------------------------------
+   quuz = make_udf(1.250000, 5.000000, 1337)
+        = UserDefined(1.250000, 5.000000, 1337)
+   ------------------------------------------------------------
+   just_print()
+    ======== BEGIN FORTRAN ========
+    just_print() was called
+    ========  END  FORTRAN ========
